dc58bebe9de89f295e34811f8f32eea1b5a2b392
Joyce Pebesma <>
2018-05-29 12:05:23 +0200

    add sound

--- Assets/Scripts/TriggerReactions.cs
+++ Assets/Scripts/TriggerReactions.cs
@@ -2,49 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerReactions : MonoBehaviour {
+public class TriggerReactions : MonoBehaviour
+{
 
-    public GameObject chair;
-    public GameObject oilLamp;
+	public AudioClip chairClip;
+	public AudioClip lampClip;
+	public GameObject chair;
+	public GameObject oilLamp;
 
-    private Vector3 lastLocationChair;
-    private Vector3 currentLocationChair;
+	private Vector3 lastLocationChair;
+	private Vector3 currentLocationChair;
 
-    private Vector3 lastLocationOilLamp;
-    private Vector3 currentLocationOilLamp;
+	private Vector3 lastLocationOilLamp;
+	private Vector3 currentLocationOilLamp;
 
+	private AudioSource source;
+	private float vol;
 
-    private int nextUpdate = 1;
+	private int nextUpdate = 1;
 
-    // Use this for initialization
-    void Start () {
-        lastLocationChair = chair.transform.position;
-        lastLocationOilLamp = oilLamp.transform.position;
+	// Use this for initialization
+	void Start ()
+	{
+		lastLocationChair = chair.transform.position;
+		lastLocationOilLamp = oilLamp.transform.position;
+
+		source = GetComponent<AudioSource> ();
+		vol = Random.Range (.5f, 1.0f);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Time.time >= nextUpdate) {
-            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
-            UpdateEverySecond();
-        }
-    }
+	void Update ()
+	{
+		if (Time.time >= nextUpdate) {
+			nextUpdate = Mathf.FloorToInt (Time.time) + 1;
+			UpdateEverySecond ();
+		}
+	}
 
-    void UpdateEverySecond() {
-        currentLocationChair = chair.transform.position;
-        currentLocationOilLamp = oilLamp.transform.position;
+	void UpdateEverySecond ()
+	{
+		currentLocationChair = chair.transform.position;
+		currentLocationOilLamp = oilLamp.transform.position;
 
-        bool movedChair = checkMovement(currentLocationChair, lastLocationChair);
-        bool movedLamp = checkMovement(currentLocationOilLamp, lastLocationOilLamp);
+		bool movedChair = checkMovement (currentLocationChair, lastLocationChair);
+		bool movedLamp = checkMovement (currentLocationOilLamp, lastLocationOilLamp);
 
-        lastLocationChair = currentLocationChair;
-        lastLocationOilLamp = currentLocationOilLamp;
-    }
+		if (movedChair) {
+			chairTriggered ();
+		}
+		if (movedLamp) {
+			lampTriggered ();
+		}
 
-    bool checkMovement(Vector3 current, Vector3 last) {
-        float dist = Vector3.Distance(current, last);
-        float angle = Vector3.Angle(current, last);
+		lastLocationChair = currentLocationChair;
+		lastLocationOilLamp = currentLocationOilLamp;
+	}
 
-        return (dist > 0.5 || angle > 0.5);
-    }
+	bool checkMovement (Vector3 current, Vector3 last)
+	{
+		float dist = Vector3.Distance (current, last);
+		float angle = Vector3.Angle (current, last);
+
+		return (dist > 0.5 || angle > 0.5);
+	}
+
+
+
+	void chairTriggered ()
+	{
+		source.PlayOneShot (chairSource, vol);
+	}
+
+	void lampTriggered ()
+	{
+		source.PlayOneShot (lampSource, vol);
+	}
 }
